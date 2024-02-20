594a1b70d5839dc7d877aabd40112a719a91d265
Ernesto Flores <eflores73@students.avc.edu>
2024-02-20 15:12:05 -0800

    testing

--- main_read.cpp
+++ main_read.cpp
@@ -4,7 +4,28 @@ using namespace std;
 
 int main()
 {
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+	ifstream inFile("data.txt");
+	int N;
+	string name;
+	int score1, score2;
+
+	if (!inFile) {
+		cout << "Unable to open file" << endl;
+		return 1;
+	}
+
+	inFile >> N;
+	inFile.ignore();
+
+	for (int i = 0; i < N; i++) {
+		getline(inFile, name, ' ');
+		inFile >>  score1 >> score2;
+		inFile.ignore();
+		int sum = score1 + score2;
+		double avg = sum / 2.0;
+		cout << "Student name: " << name << "     score1: " << score1 << "     score2: " << score2 << "     Sum: " << sum << "     Avg: " << avg << endl;
+
+	}
+	inFile.close();
+	return 0;
 }
--- main_write.cpp
+++ main_write.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 int main()
 {
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+   ofstream outFile("data.txt");
+   int N;
+   string name;
+   int score1, score2;
+
+   cout << "Enter the number of students: ";
+   cin >> N;
+   outFile << N << endl;
+
+   for (int i = 0; i < N; i++) {
+    cout << "Enter student's name and two scores: ";
+    cin >> ws;
+    getline(cin, name);
+    cin >> score1 >> score2;
+    outFile << name << " " << score1 << " " << score2 << endl;
+
+   }
+   outFile.close();
+   return 0;
 }
